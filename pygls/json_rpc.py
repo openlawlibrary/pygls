da614c81bdb5ea6874972089c7b83fd37c32ce83
Daniel Elero <danixeee@gmail.com>
2018-10-02 00:46:19 +0200

    Add notification and request handlers

--- pygls/json_rpc.py
+++ pygls/json_rpc.py
@@ -25,7 +25,16 @@ class JsonRPCRequestMessage:
         return cls(**data)
 
 
+class JsonRPCResponseMessage:
+    def __init__(self, id=None, jsonrpc=None, result=None, error=None):
+        self.id = id
+        self.version = jsonrpc
+        self.result = result
+        self.error = error
+
+
 class JsonRPCProtocol(asyncio.Protocol):
+    CANCEL_METHOD = "$/cancelRequest"
     DEFAULT_CHARSET = "utf-8"
     DEFAULT_CONTENT_TYPE = "application/jsonrpc"
     VERSION = "2.0"
@@ -40,8 +49,69 @@ class JsonRPCProtocol(asyncio.Protocol):
         self.procedures = {}
         self.transport = None
 
+        self._client_request_futures = {}
+        self._server_request_futures = {}
+
+        self._pool = ThreadPool()
+
     def __call__(self):
         return self
+
+    def _handle_cancel_notification(self, msg_id):
+        """Handle a cancel notification from the client."""
+        request_future = self._client_request_futures.pop(msg_id, None)
+
+        if not request_future:
+            logger.warn("Cancel notification for unknown message id {}"
+                        .format(msg_id))
+            return
+
+        # Will only work if the request hasn't started executing
+        if request_future.cancel():
+            logger.debug("Cancelled request with id {}".format(msg_id))
+
+    def _handle_notification(self, method_name, params):
+        """Handle a notification from the client."""
+        if method_name == JsonRPCProtocol.CANCEL_METHOD:
+            self._handle_cancel_notification(params.get('id'))
+            return
+
+        try:
+            handler = self.procedures[method_name]
+
+            if asyncio.iscoroutinefunction(handler):
+                asyncio.ensure_future(handler(params))
+            else:
+                self._pool.apply_async(handler, (params, ))
+
+        except KeyError:
+            logger.warn("Ignoring notification for unknown method {}"
+                        .format(method_name))
+        except Exception:
+            logger.exception("Failed to handle notification {}: {}"
+                             .format(method_name, params))
+
+    def _handle_request(self, msg_id, method_name, params):
+        """Handle a request from the client."""
+        try:
+            handler = self.procedures[method_name]
+
+            if asyncio.iscoroutinefunction(handler):
+                future = asyncio.ensure_future(handler(params))
+                self._client_request_futures[msg_id] = future
+                future.add_done_callback(lambda res: self.send_data(res.result()))
+            else:
+                # Can't be canceled
+                self._pool.apply_async(
+                    handler,
+                    callback=lambda res: self.send_data(
+                        JsonRPCResponseMessage(msg_id,
+                                               JsonRPCProtocol.VERSION,
+                                               res,
+                                               None)))
+        except Exception:
+            logger.exception("Failed to handle request {} {} {}"
+                             .format(msg_id, method_name, params))
 
     def _procedure_handler(self, message: JsonRPCRequestMessage):
         if message.version != JsonRPCProtocol.VERSION:
@@ -50,15 +120,17 @@ class JsonRPCProtocol(asyncio.Protocol):
 
         if message.id is None:
             logger.debug("Notification message received.")
+            self._handle_notification(message.method, message.params)
         elif message.method is None:
             logger.debug("Response message received.")
         else:
             logger.debug("Request message received.")
+            self._handle_request(message.id, message.method, message.params)
 
-    def connection_made(self, transport):
+    def connection_made(self, transport: asyncio.Transport):
         self.transport = transport
 
-    def data_received(self, data):
+    def data_received(self, data: bytes):
         logger.debug("Received {}".format(data))
 
         if not data:
@@ -71,7 +143,7 @@ class JsonRPCProtocol(asyncio.Protocol):
         except:
             pass
 
-    def register_procedure(self, procedure_name, **options):
+    def register_procedure(self, procedure_name: str, **options: dict):
         def decorator(f):
             if procedure_name in self.procedures:
                 msg = "Procedure {} is already registered." \
@@ -93,8 +165,8 @@ class JsonRPCProtocol(asyncio.Protocol):
 
     def send_data(self, data):
         try:
-            content_length = len(data.encode("utf-8")) if data else 0
             body = json.dumps(data, default=lambda o: o.__dict__)
+            content_length = len(body.encode("utf-8")) if body else 0
 
             response = (
                 "Content-Length: {}\r\n"
