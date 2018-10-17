fec749fa0a51f4396f11c8b5f6a39f4ebf09145c
danixeee <danixeee@gmail.com>
2018-10-17 21:26:46 +0200

    Consume and write data using stdin/stdout

--- pygls/server.py
+++ pygls/server.py
@@ -1,28 +1,91 @@
 import asyncio
 import logging
+import os
+import sys
+from re import findall
 
+from . import IS_WIN
 from .protocol import LanguageServerProtocol
 
 logger = logging.getLogger(__name__)
 
 
+async def aio_readline(loop, proxy):
+    '''
+    Read data from stdin in separate thread (asynchronously)
+    '''
+    while True:
+        # Read line
+        line = await loop.run_in_executor(None, sys.stdin.readline)
+
+        if not line:
+            continue
+
+        # Extract content length from line
+        try:
+            content_length = int(findall(r'\b\d+\b', line)[0])
+            logger.info(content_length)
+        except:
+            continue
+
+        # Throw away empty lines
+        while line and line.strip():
+            line = await loop.run_in_executor(None, sys.stdin.readline)
+
+        if not line:
+            continue
+
+        # Read body
+        body = await loop.run_in_executor(None, sys.stdin.read, content_length)
+
+        # Pass body to language server protocol
+        if body:
+            proxy(body.encode('utf-8'))
+
+
+class StdOutTransportAdapter(asyncio.Transport):
+    '''
+    Protocol adapter which overrides write method.
+    Write method sends data to stdout.
+    '''
+
+    def close(self):
+        sys.stdout.buffer.close()
+
+    def write(self, data):
+        sys.stdout.buffer.write(data)
+        sys.stdout.buffer.flush()
+
+
 class Server:
     def __init__(self, protocol_cls):
         assert issubclass(protocol_cls, asyncio.Protocol)
+
+        if IS_WIN:
+            asyncio.set_event_loop(asyncio.ProactorEventLoop())
+
         self.loop = asyncio.get_event_loop()
 
         self.lsp = protocol_cls(self)
         self.server = None
 
     def shutdown(self):
-        self.server.close()
+        # self.server.close()
         # TODO: Gracefully shutdown event loops
+        pass
 
     def start_tcp(self, host, port):
         self.server = self.loop.run_until_complete(
             self.loop.create_server(self.lsp, host, port)
         )
         self.loop.run_forever()
+
+    def start_io(self):
+        transport = StdOutTransportAdapter()
+        self.lsp.connection_made(transport)
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(aio_readline(loop, self.lsp.data_received))
 
 
 class LanguageServer(Server):
