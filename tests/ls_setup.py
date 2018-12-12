c8ff15b170335f69b2559a1190d395ee581c4680
augb <burch.aug@gmail.com>
2018-12-12 16:40:00 -0600

    Add Apache 2.0 license.

--- tests/ls_setup.py
+++ tests/ls_setup.py
@@ -1,7 +1,19 @@
-##########################################################################
-# Copyright (c) Open Law Library. All rights reserved.                   #
-# See ThirdPartyNotices.txt in the project root for license information. #
-##########################################################################
+############################################################################
+# Copyright(c) Open Law Library. All rights reserved.                      #
+# See ThirdPartyNotices.txt in the project root for additional notices.    #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License")           #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http: // www.apache.org/licenses/LICENSE-2.0                         #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
 import threading
 
 from tests import (CMD_ASYNC, CMD_SYNC, CMD_THREAD, FEATURE_ASYNC,
