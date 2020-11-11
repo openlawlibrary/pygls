d0157a5d99cc9812b3b9113b49e032e83da9c0f6
Daniel Elero <danixeee@gmail.com>
2020-11-11 13:52:39 +0100

    Add test for code completions

---
+++ tests/lsp/test_code_completion.py
@@ -0,0 +1,52 @@
+import unittest
+
+from pygls.lsp.methods import COMPLETION
+from pygls.lsp.types import (CompletionItem, CompletionItemKind, CompletionList, CompletionOptions,
+                             CompletionParams, Position, TextDocumentIdentifier)
+from pygls.server import LanguageServer
+
+from ..conftest import CALL_TIMEOUT, ClientServer
+
+
+class TestCompletions(unittest.TestCase):
+    def setUp(self):
+        self.client_server = ClientServer()
+        self.client, self.server = self.client_server
+
+        @self.server.feature(COMPLETION, CompletionOptions(trigger_characters=[',']))
+        def completions(params: CompletionParams) -> CompletionList:
+            return CompletionList(
+                is_incomplete=False,
+                items=[
+                    CompletionItem(
+                        label='test1',
+                        kind=CompletionItemKind.Method
+                    ),
+                ]
+            )
+
+        self.client_server.start()
+
+
+    def tearDown(self):
+        self.client_server.stop()
+
+    def test_completions(self):
+        response = self.client.lsp.send_request(
+            COMPLETION,
+            CompletionParams(
+                text_document=TextDocumentIdentifier(uri='file://test.test'),
+                position=Position(line=0, character=0)
+            )
+        ).result(timeout=CALL_TIMEOUT)
+
+        assert response['isIncomplete'] == False
+        assert response['items'][0]['label'] == 'test1'
+        assert response['items'][0]['kind'] == CompletionItemKind.Method
+        assert response['items'][0]['preselect'] == False
+        assert response['items'][0]['deprecated'] == False
+
+
+
+if __name__ == '__main__':
+    unittest.main()
