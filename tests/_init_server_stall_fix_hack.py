1b13794d38439fd01f0215ca5c20b82eede84a13
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-07-06 22:35:18 -0400

    fix: Retry timedout test server init
    This should finally resolve our annoying flakey test errors. Of course
    it's not the real fix, but it's preferable to actually get the suite to
    be stable. Besides this isn't part of the production server, it's just
    for testing.

---
+++ tests/_init_server_stall_fix_hack.py
@@ -0,0 +1,27 @@
+"""
+It would be great to find the real underlying issue here, but without these
+retries we get annoying flakey test errors. So it's preferable to hack this
+fix to actually guarantee it doesn't generate false negatives in the test
+suite.
+"""
+import concurrent
+
+RETRIES = 3
+
+
+def retry_stalled_init_fix_hack():
+    def decorator(func):
+        def newfn(*args, **kwargs):
+            attempt = 0
+            while attempt < RETRIES:
+                try:
+                    return func(*args, **kwargs)
+                except concurrent.futures._base.TimeoutError:
+                    print(
+                        '\n\nRetrying timeouted test server init '
+                        '%d of %d\n' % (attempt, RETRIES)
+                    )
+                    attempt += 1
+            return func(*args, **kwargs)
+        return newfn
+    return decorator
