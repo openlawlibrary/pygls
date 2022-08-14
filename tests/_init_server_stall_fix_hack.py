66f81da3948dde952dfbfe05dc17849758e9ee4d
Alex Carney <alcarneyme@gmail.com>
2022-08-14 16:40:34 +0100

    Make it possible to disable the testsuite timeout
    The timeouts can get in the way when trying to debug the code under
    test. This commit makes it possible to disable the timeout by running
    the testsuite with the `DISABLE_TIMEOUT` environment variable set e.g.
    
       $ DISABLE_TIMEOUT=1 pytest -x tests/

--- tests/_init_server_stall_fix_hack.py
+++ tests/_init_server_stall_fix_hack.py
@@ -4,12 +4,17 @@ retries we get annoying flakey test errors. So it's preferable to hack this
 fix to actually guarantee it doesn't generate false negatives in the test
 suite.
 """
+import os
 import concurrent
 
 RETRIES = 3
 
 
 def retry_stalled_init_fix_hack():
+
+    if 'DISABLE_TIMEOUT' in os.environ:
+        return lambda f: f
+
     def decorator(func):
         def newfn(*args, **kwargs):
             attempt = 0
