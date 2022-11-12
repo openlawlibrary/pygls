9b583cf09e1f40dda8b3fac79ea1b1ec0e019465
Alex Carney <alcarneyme@gmail.com>
2022-11-12 22:36:09 +0000

    Fix construction of `workspace.fileOperations` server capabilities

--- tests/test_capabilities.py
+++ tests/test_capabilities.py
@@ -7,20 +7,31 @@ from lsprotocol.types import (
     TEXT_DOCUMENT_DID_SAVE,
     TEXT_DOCUMENT_WILL_SAVE,
     TEXT_DOCUMENT_WILL_SAVE_WAIT_UNTIL,
+    WORKSPACE_DID_CREATE_FILES,
+    WORKSPACE_WILL_CREATE_FILES,
+    WORKSPACE_DID_DELETE_FILES,
+    WORKSPACE_WILL_DELETE_FILES,
+    WORKSPACE_DID_RENAME_FILES,
+    WORKSPACE_WILL_RENAME_FILES,
     ClientCapabilities,
+    FileOperationClientCapabilities,
+    FileOperationFilter,
+    FileOperationOptions,
+    FileOperationPattern,
+    FileOperationRegistrationOptions,
     SaveOptions,
     TextDocumentClientCapabilities,
     TextDocumentSaveRegistrationOptions,
     TextDocumentSyncClientCapabilities,
     TextDocumentSyncKind,
     TextDocumentSyncOptions,
+    WorkspaceClientCapabilities,
 )
 
 from pygls.capabilities import ServerCapabilitiesBuilder
 
 
 @pytest.mark.parametrize("capabilities,features,options,expected", [
-
     # textDocument/didOpen
     (
         ClientCapabilities(),
@@ -133,4 +144,290 @@ def test_text_doc_sync_capabilities(
     )
 
     actual = builder.build().text_document_sync
+    assert expected == actual
+
+
+@pytest.mark.parametrize("capabilities,features,options,expected", [
+    (
+        ClientCapabilities(),
+        set(),
+        {},
+        FileOperationOptions()
+    ),
+    # workspace/willCreateFiles
+    (
+        ClientCapabilities(),
+        {WORKSPACE_WILL_CREATE_FILES},
+        {
+            WORKSPACE_WILL_CREATE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions()
+    ),
+    (
+        ClientCapabilities(
+            workspace=WorkspaceClientCapabilities(
+                file_operations=FileOperationClientCapabilities(
+                    will_create=True
+                )
+            )
+        ),
+        {WORKSPACE_WILL_CREATE_FILES},
+        {
+            WORKSPACE_WILL_CREATE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions(
+            will_create=FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        )
+    ),
+    # workspace/didCreateFiles
+    (
+        ClientCapabilities(),
+        {WORKSPACE_DID_CREATE_FILES},
+        {
+            WORKSPACE_DID_CREATE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions()
+    ),
+    (
+        ClientCapabilities(
+            workspace=WorkspaceClientCapabilities(
+                file_operations=FileOperationClientCapabilities(
+                    did_create=True
+                )
+            )
+        ),
+        {WORKSPACE_DID_CREATE_FILES},
+        {
+            WORKSPACE_DID_CREATE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions(
+            did_create=FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        )
+    ),
+    # workspace/willDeleteFiles
+    (
+        ClientCapabilities(),
+        {WORKSPACE_WILL_DELETE_FILES},
+        {
+            WORKSPACE_WILL_DELETE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions()
+    ),
+    (
+        ClientCapabilities(
+            workspace=WorkspaceClientCapabilities(
+                file_operations=FileOperationClientCapabilities(
+                    will_delete=True
+                )
+            )
+        ),
+        {WORKSPACE_WILL_DELETE_FILES},
+        {
+            WORKSPACE_WILL_DELETE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions(
+            will_delete=FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        )
+    ),
+    # workspace/didDeleteFiles
+    (
+        ClientCapabilities(),
+        {WORKSPACE_DID_DELETE_FILES},
+        {
+            WORKSPACE_DID_DELETE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions()
+    ),
+    (
+        ClientCapabilities(
+            workspace=WorkspaceClientCapabilities(
+                file_operations=FileOperationClientCapabilities(
+                    did_delete=True
+                )
+            )
+        ),
+        {WORKSPACE_DID_DELETE_FILES},
+        {
+            WORKSPACE_DID_DELETE_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions(
+            did_delete=FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        )
+    ),
+    # workspace/willRenameFiles
+    (
+        ClientCapabilities(),
+        {WORKSPACE_WILL_RENAME_FILES},
+        {
+            WORKSPACE_WILL_RENAME_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions()
+    ),
+    (
+        ClientCapabilities(
+            workspace=WorkspaceClientCapabilities(
+                file_operations=FileOperationClientCapabilities(
+                    will_rename=True
+                )
+            )
+        ),
+        {WORKSPACE_WILL_RENAME_FILES},
+        {
+            WORKSPACE_WILL_RENAME_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions(
+            will_rename=FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        )
+    ),
+    # workspace/didRenameFiles
+    (
+        ClientCapabilities(),
+        {WORKSPACE_DID_RENAME_FILES},
+        {
+            WORKSPACE_DID_RENAME_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions()
+    ),
+    (
+        ClientCapabilities(
+            workspace=WorkspaceClientCapabilities(
+                file_operations=FileOperationClientCapabilities(
+                    did_rename=True
+                )
+            )
+        ),
+        {WORKSPACE_DID_RENAME_FILES},
+        {
+            WORKSPACE_DID_RENAME_FILES: FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        },
+        FileOperationOptions(
+            did_rename=FileOperationRegistrationOptions(
+                filters=[
+                    FileOperationFilter(
+                        pattern=FileOperationPattern(glob="**/*.py")
+                    )
+                ]
+            )
+        )
+    ),
+])
+def test_file_operations_capabilities(
+    capabilities: ClientCapabilities,
+    features: Set[str],
+    options,
+    expected: FileOperationOptions
+):
+    """Ensure that `pygls` can correctly construct server capabilities for the file
+    operations set of features."""
+
+    builder = ServerCapabilitiesBuilder(
+        capabilities, features, options, [], TextDocumentSyncKind.Incremental
+    )
+
+    server = builder.build()
+    assert server.workspace is not None
+
+    actual = server.workspace.file_operations
     assert expected == actual
