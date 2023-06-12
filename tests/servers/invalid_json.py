f0fab95008318017788fa83d0bcc67d5a3158e40
Alex Carney <alcarneyme@gmail.com>
2023-06-12 20:16:06 +0100

    Encode bytes before printing to stdout
    Not sure why, but this seems to be necessary to ensure the messages
    are formatted correctly on Windows

--- tests/servers/invalid_json.py
+++ tests/servers/invalid_json.py
@@ -7,7 +7,9 @@ from concurrent.futures import ThreadPoolExecutor
 from pygls.server import aio_readline
 
 def handler(data):
-    print('Content-Length: 5\r\n\r\n{"ll}', flush=True)
+    content = 'Content-Length: 5\r\n\r\n{"ll}'.encode('utf8')
+    sys.stdout.buffer.write(content)
+    sys.stdout.flush()
 
 
 async def main():
