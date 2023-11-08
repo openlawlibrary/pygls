ccc08f69f76fcd9290e940b039dae1ee05688c0f
Alex Carney <alcarneyme@gmail.com>
2023-11-08 18:25:57 +0000

    refactor: move workspace/ into servers/ dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -68,7 +68,9 @@ def client_server(request):
 @pytest.fixture(scope="session")
 def uri_for():
     """Returns the uri corresponsing to a file in the example workspace."""
-    base_dir = pathlib.Path(__file__, "..", "..", "examples", "workspace").resolve()
+    base_dir = pathlib.Path(
+        __file__, "..", "..", "examples", "servers", "workspace"
+    ).resolve()
 
     def fn(*args):
         fpath = pathlib.Path(base_dir, *args)
