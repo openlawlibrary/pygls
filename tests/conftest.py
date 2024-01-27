4e7bb8fe40133818f35e30693307d105130bf0c1
Alex Carney <alcarneyme@gmail.com>
2024-01-27 00:45:36 +0000

    refactor: test infrastructure
    This commit refactors the infrastructure for writing end-to-end tests.
    
    There are now two fixtures `get_client_for` and `uri_for`, as the
    names suggest these are functions for obtaining a `LanguageClient`
    connected to a server in `examples/servers` and URIs for files in
    `examples/servers/workspace` respectively.
    
    This also adds a custom cli flag to our test suite - `--lsp-runtime`
    which allows to choose whether the end-to-end tests are run against
    the `cpython`, `pyodide` or `wasi` runtimes.
    
    The fixtures mentioned above, take the value of the `--lsp-runtime`
    flag into account, allowing for the same test code to work against
    each of the supported runtimes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,21 +18,14 @@
 ############################################################################
 import asyncio
 import pathlib
+import sys
 
 import pytest
-from lsprotocol import types, converters
+from lsprotocol import types
 
-from pygls import uris, IS_PYODIDE
-from pygls.feature_manager import FeatureManager
+from pygls import uris
+from pygls.lsp.client import LanguageClient as LanguageClient_
 from pygls.workspace import Workspace
-
-from .ls_setup import (
-    NativeClientServer,
-    PyodideClientServer,
-    setup_ls_features,
-)
-
-from .client import create_client_for_server
 
 DOC = """document
 for
@@ -40,59 +33,82 @@ testing
 with "😋" unicode.
 """
 DOC_URI = uris.from_fs_path(__file__) or ""
+REPO_DIR = pathlib.Path(__file__, "..", "..").resolve()
+SERVER_DIR = REPO_DIR / "examples" / "servers"
+WORKSPACE_DIR = REPO_DIR / "examples" / "servers" / "workspace"
+WASI_DIR = REPO_DIR / "tests" / "wasi"
 
 
-ClientServer = NativeClientServer
-if IS_PYODIDE:
-    ClientServer = PyodideClientServer
+class LanguageClient(LanguageClient_):
+    """Language client to use for testing."""
+
+    async def server_exit(self, server: asyncio.subprocess.Process):
+        # -15: terminated (probably by the client)
+        #   0: all ok
+        if server.returncode not in {-15, 0}:
+            if server.stderr is not None:
+                err = await server.stderr.read()
+                self.logger.debug("stderr:\n%s", err.decode("utf8"))
 
 
-@pytest.fixture(autouse=False)
-def client_server(request):
-    if hasattr(request, "param"):
-        ConfiguredClientServer = request.param
-        client_server = ConfiguredClientServer()
-    else:
-        client_server = ClientServer()
-        setup_ls_features(client_server.server)
-
-    client_server.start()
-    client, server = client_server
-
-    yield client, server
-
-    client_server.stop()
+def pytest_addoption(parser):
+    """Add extra cli arguments to pytest."""
+    group = parser.getgroup("pygls")
+    group.addoption(
+        "--lsp-runtime",
+        dest="lsp_runtime",
+        action="store",
+        default="cpython",
+        choices=("cpython", "pyodide", "wasi"),
+        help="Choose the runtime in which to run servers under test.",
+    )
 
 
 @pytest.fixture(scope="session")
-def uri_for():
-    """Returns the uri corresponsing to a file in the example workspace."""
-    base_dir = pathlib.Path(
-        __file__, "..", "..", "examples", "servers", "workspace"
-    ).resolve()
+def runtime(request):
+    """This fixture is the source of truth as to which environment we should run the
+    end-to-end tests in."""
+    return request.config.getoption("lsp_runtime")
+
+
+@pytest.fixture(scope="session")
+def path_for():
+    """Returns the path corresponding to a file in the example workspace"""
 
     def fn(*args):
-        fpath = pathlib.Path(base_dir, *args)
-        return uris.from_fs_path(str(fpath))
+        fpath = pathlib.Path(WORKSPACE_DIR, *args)
+        assert fpath.exists()
+
+        return fpath
 
     return fn
 
 
-@pytest.fixture()
-def event_loop():
-    """Redefine `pytest-asyncio's default event_loop fixture to match the scope
-    of our client fixture."""
+@pytest.fixture(scope="session")
+def uri_for(runtime, path_for):
+    """Returns the uri corresponsing to a file in the example workspace.
 
-    policy = asyncio.get_event_loop_policy()
+    Takes into account the runtime.
+    """
 
-    loop = policy.new_event_loop()
-    yield loop
+    def fn(*args):
+        fpath = path_for(*args)
 
-    try:
-        # Not implemented on pyodide
-        loop.close()
-    except NotImplementedError:
-        pass
+        if runtime == "pyodide":
+            # Pyodide cannot see the whole file system, so this needs to be made relative to
+            # the workspace's parent folder
+            path = str(fpath).replace(str(WORKSPACE_DIR.parent), "")
+            return uris.from_fs_path(path)
+
+        elif runtime == "wasi":
+            # WASI cannot see the whole filesystem, so this needs to be made relative to
+            # the repo root
+            path = str(fpath).replace(str(REPO_DIR), "")
+            return uris.from_fs_path(path)
+
+        return uris.from_fs_path(str(fpath))
+
+    return fn
 
 
 @pytest.fixture(scope="session")
@@ -102,15 +118,125 @@ def server_dir():
     return path.resolve()
 
 
-code_action_client = create_client_for_server("code_actions.py")
-inlay_hints_client = create_client_for_server("inlay_hints.py")
-json_server_client = create_client_for_server("json_server.py")
+def get_client_for_cpython_server(uri_fixture):
+    """Return a client configured to communicate with a server running under cpython."""
+
+    async def fn(server_name: str):
+        client = LanguageClient("pygls-test-suite", "v1")
+        await client.start_io(sys.executable, str(SERVER_DIR / server_name))
+
+        response = await client.initialize(
+            types.InitializeParams(
+                capabilities=types.ClientCapabilities(),
+                root_uri=uri_fixture(""),
+            )
+        )
+        assert response is not None
+        return client, response
+
+    return fn
 
 
-@pytest.fixture
-def feature_manager():
-    """Return a feature manager"""
-    return FeatureManager(None, converters.get_converter())
+def get_client_for_pyodide_server(uri_fixture):
+    """Return a client configured to communicate with a server running under Pyodide.
+
+    This assumes that the pyodide environment has already been bootstrapped.
+    """
+
+    async def fn(server_name: str):
+        client = LanguageClient("pygls-test-suite", "v1")
+
+        PYODIDE_DIR = REPO_DIR / "tests" / "pyodide"
+        server_py = str(SERVER_DIR / server_name)
+
+        await client.start_io("node", str(PYODIDE_DIR / "run_server.js"), server_py)
+
+        response = await client.initialize(
+            types.InitializeParams(
+                capabilities=types.ClientCapabilities(),
+                root_uri=uri_fixture(""),
+            )
+        )
+        assert response is not None
+        return client, response
+
+    return fn
+
+
+def get_client_for_wasi_server(uri_fixture):
+    """Return a client configured to communicate with a server running under WASI.
+
+    This assumes the ``wasmtime`` executable is available to be used as the WASI host.
+    """
+
+    async def fn(server_name: str):
+        client = LanguageClient("pygls-test-suite", "v1")
+
+        # WASI cannot see the whole filesystem, so this needs to be made relative to the
+        # repo root
+        server_py = str(SERVER_DIR / server_name).replace(str(REPO_DIR), "")
+
+        # fmt: off
+        await client.start_io(
+            "wasmtime", "run",
+            # Tell python where its standard library lives and grant access to it.
+            "--env", f"PYTHONHOME={WASI_DIR}",
+            "--dir", str(WASI_DIR),
+            # Grant access to the current working directory
+            "--dir", ".",
+            str(WASI_DIR / "python.wasm"),
+            # Everything from here will be passed to Python itself
+            server_py,
+        )
+        # fmt: on
+
+        response = await client.initialize(
+            types.InitializeParams(
+                capabilities=types.ClientCapabilities(),
+                root_uri=uri_fixture(""),
+            )
+        )
+        assert response is not None
+        return client, response
+
+    return fn
+
+
+@pytest.fixture(scope="session")
+def get_client_for(runtime, uri_for):
+    """Return a client configured to communicate with the specified server.
+
+    Takes into account the current runtime.
+
+    It's the consuming fixture's responsibility to stop the client.
+    """
+    # TODO: Add TCP/WS support.
+    if runtime not in {"cpython", "pyodide", "wasi"}:
+        raise NotImplementedError(f"get_client_for: {runtime=}")
+
+    elif runtime == "pyodide":
+        return get_client_for_pyodide_server(uri_for)
+
+    elif runtime == "wasi":
+        return get_client_for_wasi_server(uri_for)
+
+    return get_client_for_cpython_server(uri_for)
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    """Redefine `pytest-asyncio's default event_loop fixture to match the scope
+    of our client fixture."""
+    # TODO: Remove for pytest-asyncio 0.23.x
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+
+    try:
+        # Not implemented for pyodide's event loop
+        loop.close()
+    except NotImplementedError:
+        pass
 
 
 @pytest.fixture
