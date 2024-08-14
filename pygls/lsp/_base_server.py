d87b95febfd2ded8086a8873626776c6d374e2f6
Alex Carney <alcarneyme@gmail.com>
2024-08-14 19:49:11 +0100

    feat: generate server methods from lsp types
    Similarly to the `BaseLanguageClient` this commit now introduces a
    `BaseLanguageServer` with methods automatically generated from type
    definitions provided by `lsprotocol`

---
+++ pygls/lsp/_base_server.py
@@ -0,0 +1,444 @@
+# GENERATED FROM scripts/generate_client_server.py -- DO NOT EDIT
+# flake8: noqa
+from cattrs import Converter
+from concurrent.futures import Future
+from lsprotocol import types
+from pygls.protocol import LanguageServerProtocol
+from pygls.protocol import default_converter
+from pygls.server import Server
+import typing
+from typing import Any
+from typing import Callable
+from typing import Optional
+from typing import Sequence
+from typing import Type
+
+
+class BaseLanguageServer(Server):
+
+    protocol: LanguageServerProtocol
+
+    def __init__(
+        self,
+        protocol_cls: Type[LanguageServerProtocol] = LanguageServerProtocol,
+        converter_factory: Callable[[], Converter] = default_converter,
+        loop=None,
+        **kwargs,
+    ):
+        super().__init__(protocol_cls, converter_factory, loop, **kwargs)
+
+    def client_register_capability(
+        self,
+        params: types.RegistrationParams,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`client/registerCapability` request.
+
+        The `client/registerCapability` request is sent from the server to the client to register a new capability
+        handler on the client side.
+        """
+        return self.protocol.send_request("client/registerCapability", params, callback)
+
+    async def client_register_capability_async(
+        self,
+        params: types.RegistrationParams,
+    ) -> None:
+        """Make a :lsp:`client/registerCapability` request.
+
+        The `client/registerCapability` request is sent from the server to the client to register a new capability
+        handler on the client side.
+        """
+        return await self.protocol.send_request_async("client/registerCapability", params)
+
+    def client_unregister_capability(
+        self,
+        params: types.UnregistrationParams,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`client/unregisterCapability` request.
+
+        The `client/unregisterCapability` request is sent from the server to the client to unregister a previously registered capability
+        handler on the client side.
+        """
+        return self.protocol.send_request("client/unregisterCapability", params, callback)
+
+    async def client_unregister_capability_async(
+        self,
+        params: types.UnregistrationParams,
+    ) -> None:
+        """Make a :lsp:`client/unregisterCapability` request.
+
+        The `client/unregisterCapability` request is sent from the server to the client to unregister a previously registered capability
+        handler on the client side.
+        """
+        return await self.protocol.send_request_async("client/unregisterCapability", params)
+
+    def window_show_document(
+        self,
+        params: types.ShowDocumentParams,
+        callback: Optional[Callable[[types.ShowDocumentResult], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`window/showDocument` request.
+
+        A request to show a document. This request might open an
+        external program depending on the value of the URI to open.
+        For example a request to open `https://code.visualstudio.com/`
+        will very likely open the URI in a WEB browser.
+
+        @since 3.16.0
+        """
+        return self.protocol.send_request("window/showDocument", params, callback)
+
+    async def window_show_document_async(
+        self,
+        params: types.ShowDocumentParams,
+    ) -> types.ShowDocumentResult:
+        """Make a :lsp:`window/showDocument` request.
+
+        A request to show a document. This request might open an
+        external program depending on the value of the URI to open.
+        For example a request to open `https://code.visualstudio.com/`
+        will very likely open the URI in a WEB browser.
+
+        @since 3.16.0
+        """
+        return await self.protocol.send_request_async("window/showDocument", params)
+
+    def window_show_message_request(
+        self,
+        params: types.ShowMessageRequestParams,
+        callback: Optional[Callable[[Optional[types.MessageActionItem]], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`window/showMessageRequest` request.
+
+        The show message request is sent from the server to the client to show a message
+        and a set of options actions to the user.
+        """
+        return self.protocol.send_request("window/showMessageRequest", params, callback)
+
+    async def window_show_message_request_async(
+        self,
+        params: types.ShowMessageRequestParams,
+    ) -> Optional[types.MessageActionItem]:
+        """Make a :lsp:`window/showMessageRequest` request.
+
+        The show message request is sent from the server to the client to show a message
+        and a set of options actions to the user.
+        """
+        return await self.protocol.send_request_async("window/showMessageRequest", params)
+
+    def window_work_done_progress_create(
+        self,
+        params: types.WorkDoneProgressCreateParams,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`window/workDoneProgress/create` request.
+
+        The `window/workDoneProgress/create` request is sent from the server to the client to initiate progress
+        reporting from the server.
+        """
+        return self.protocol.send_request("window/workDoneProgress/create", params, callback)
+
+    async def window_work_done_progress_create_async(
+        self,
+        params: types.WorkDoneProgressCreateParams,
+    ) -> None:
+        """Make a :lsp:`window/workDoneProgress/create` request.
+
+        The `window/workDoneProgress/create` request is sent from the server to the client to initiate progress
+        reporting from the server.
+        """
+        return await self.protocol.send_request_async("window/workDoneProgress/create", params)
+
+    def workspace_apply_edit(
+        self,
+        params: types.ApplyWorkspaceEditParams,
+        callback: Optional[Callable[[types.ApplyWorkspaceEditResult], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/applyEdit` request.
+
+        A request sent from the server to the client to modified certain resources.
+        """
+        return self.protocol.send_request("workspace/applyEdit", params, callback)
+
+    async def workspace_apply_edit_async(
+        self,
+        params: types.ApplyWorkspaceEditParams,
+    ) -> types.ApplyWorkspaceEditResult:
+        """Make a :lsp:`workspace/applyEdit` request.
+
+        A request sent from the server to the client to modified certain resources.
+        """
+        return await self.protocol.send_request_async("workspace/applyEdit", params)
+
+    def workspace_code_lens_refresh(
+        self,
+        params: None,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/codeLens/refresh` request.
+
+        A request to refresh all code actions
+
+        @since 3.16.0
+        """
+        return self.protocol.send_request("workspace/codeLens/refresh", params, callback)
+
+    async def workspace_code_lens_refresh_async(
+        self,
+        params: None,
+    ) -> None:
+        """Make a :lsp:`workspace/codeLens/refresh` request.
+
+        A request to refresh all code actions
+
+        @since 3.16.0
+        """
+        return await self.protocol.send_request_async("workspace/codeLens/refresh", params)
+
+    def workspace_configuration(
+        self,
+        params: types.ConfigurationParams,
+        callback: Optional[Callable[[Sequence[Optional[Any]]], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/configuration` request.
+
+        The 'workspace/configuration' request is sent from the server to the client to fetch a certain
+        configuration setting.
+
+        This pull model replaces the old push model were the client signaled configuration change via an
+        event. If the server still needs to react to configuration changes (since the server caches the
+        result of `workspace/configuration` requests) the server should register for an empty configuration
+        change event and empty the cache if such an event is received.
+        """
+        return self.protocol.send_request("workspace/configuration", params, callback)
+
+    async def workspace_configuration_async(
+        self,
+        params: types.ConfigurationParams,
+    ) -> Sequence[Optional[Any]]:
+        """Make a :lsp:`workspace/configuration` request.
+
+        The 'workspace/configuration' request is sent from the server to the client to fetch a certain
+        configuration setting.
+
+        This pull model replaces the old push model were the client signaled configuration change via an
+        event. If the server still needs to react to configuration changes (since the server caches the
+        result of `workspace/configuration` requests) the server should register for an empty configuration
+        change event and empty the cache if such an event is received.
+        """
+        return await self.protocol.send_request_async("workspace/configuration", params)
+
+    def workspace_diagnostic_refresh(
+        self,
+        params: None,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/diagnostic/refresh` request.
+
+        The diagnostic refresh request definition.
+
+        @since 3.17.0
+        """
+        return self.protocol.send_request("workspace/diagnostic/refresh", params, callback)
+
+    async def workspace_diagnostic_refresh_async(
+        self,
+        params: None,
+    ) -> None:
+        """Make a :lsp:`workspace/diagnostic/refresh` request.
+
+        The diagnostic refresh request definition.
+
+        @since 3.17.0
+        """
+        return await self.protocol.send_request_async("workspace/diagnostic/refresh", params)
+
+    def workspace_folding_range_refresh(
+        self,
+        params: None,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/foldingRange/refresh` request.
+
+        @since 3.18.0
+        @proposed
+        """
+        return self.protocol.send_request("workspace/foldingRange/refresh", params, callback)
+
+    async def workspace_folding_range_refresh_async(
+        self,
+        params: None,
+    ) -> None:
+        """Make a :lsp:`workspace/foldingRange/refresh` request.
+
+        @since 3.18.0
+        @proposed
+        """
+        return await self.protocol.send_request_async("workspace/foldingRange/refresh", params)
+
+    def workspace_inlay_hint_refresh(
+        self,
+        params: None,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/inlayHint/refresh` request.
+
+        @since 3.17.0
+        """
+        return self.protocol.send_request("workspace/inlayHint/refresh", params, callback)
+
+    async def workspace_inlay_hint_refresh_async(
+        self,
+        params: None,
+    ) -> None:
+        """Make a :lsp:`workspace/inlayHint/refresh` request.
+
+        @since 3.17.0
+        """
+        return await self.protocol.send_request_async("workspace/inlayHint/refresh", params)
+
+    def workspace_inline_value_refresh(
+        self,
+        params: None,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/inlineValue/refresh` request.
+
+        @since 3.17.0
+        """
+        return self.protocol.send_request("workspace/inlineValue/refresh", params, callback)
+
+    async def workspace_inline_value_refresh_async(
+        self,
+        params: None,
+    ) -> None:
+        """Make a :lsp:`workspace/inlineValue/refresh` request.
+
+        @since 3.17.0
+        """
+        return await self.protocol.send_request_async("workspace/inlineValue/refresh", params)
+
+    def workspace_semantic_tokens_refresh(
+        self,
+        params: None,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/semanticTokens/refresh` request.
+
+        @since 3.16.0
+        """
+        return self.protocol.send_request("workspace/semanticTokens/refresh", params, callback)
+
+    async def workspace_semantic_tokens_refresh_async(
+        self,
+        params: None,
+    ) -> None:
+        """Make a :lsp:`workspace/semanticTokens/refresh` request.
+
+        @since 3.16.0
+        """
+        return await self.protocol.send_request_async("workspace/semanticTokens/refresh", params)
+
+    def workspace_text_document_content_refresh(
+        self,
+        params: types.TextDocumentContentRefreshParams,
+        callback: Optional[Callable[[None], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/textDocumentContent/refresh` request.
+
+        The `workspace/textDocumentContent` request is sent from the server to the client to refresh
+        the content of a specific text document.
+
+        @since 3.18.0
+        @proposed
+        """
+        return self.protocol.send_request("workspace/textDocumentContent/refresh", params, callback)
+
+    async def workspace_text_document_content_refresh_async(
+        self,
+        params: types.TextDocumentContentRefreshParams,
+    ) -> None:
+        """Make a :lsp:`workspace/textDocumentContent/refresh` request.
+
+        The `workspace/textDocumentContent` request is sent from the server to the client to refresh
+        the content of a specific text document.
+
+        @since 3.18.0
+        @proposed
+        """
+        return await self.protocol.send_request_async("workspace/textDocumentContent/refresh", params)
+
+    def workspace_workspace_folders(
+        self,
+        params: None,
+        callback: Optional[Callable[[Optional[Sequence[types.WorkspaceFolder]]], None]] = None,
+    ) -> Future:
+        """Make a :lsp:`workspace/workspaceFolders` request.
+
+        The `workspace/workspaceFolders` is sent from the server to the client to fetch the open workspace folders.
+        """
+        return self.protocol.send_request("workspace/workspaceFolders", params, callback)
+
+    async def workspace_workspace_folders_async(
+        self,
+        params: None,
+    ) -> Optional[Sequence[types.WorkspaceFolder]]:
+        """Make a :lsp:`workspace/workspaceFolders` request.
+
+        The `workspace/workspaceFolders` is sent from the server to the client to fetch the open workspace folders.
+        """
+        return await self.protocol.send_request_async("workspace/workspaceFolders", params)
+
+    def cancel_request(self, params: types.CancelParams) -> None:
+        """Send a :lsp:`$/cancelRequest` notification.
+
+
+        """
+        self.protocol.notify("$/cancelRequest", params)
+
+    def log_trace(self, params: types.LogTraceParams) -> None:
+        """Send a :lsp:`$/logTrace` notification.
+
+
+        """
+        self.protocol.notify("$/logTrace", params)
+
+    def progress(self, params: types.ProgressParams) -> None:
+        """Send a :lsp:`$/progress` notification.
+
+
+        """
+        self.protocol.notify("$/progress", params)
+
+    def telemetry_event(self, params: typing.Optional[typing.Any]) -> None:
+        """Send a :lsp:`telemetry/event` notification.
+
+        The telemetry event notification is sent from the server to the client to ask
+        the client to log telemetry data.
+        """
+        self.protocol.notify("telemetry/event", params)
+
+    def text_document_publish_diagnostics(self, params: types.PublishDiagnosticsParams) -> None:
+        """Send a :lsp:`textDocument/publishDiagnostics` notification.
+
+        Diagnostics notification are sent from the server to the client to signal
+        results of validation runs.
+        """
+        self.protocol.notify("textDocument/publishDiagnostics", params)
+
+    def window_log_message(self, params: types.LogMessageParams) -> None:
+        """Send a :lsp:`window/logMessage` notification.
+
+        The log message notification is sent from the server to the client to ask
+        the client to log a particular message.
+        """
+        self.protocol.notify("window/logMessage", params)
+
+    def window_show_message(self, params: types.ShowMessageParams) -> None:
+        """Send a :lsp:`window/showMessage` notification.
+
+        The show message notification is sent from a server to a client to ask
+        the client to display a particular message in the user interface.
+        """
+        self.protocol.notify("window/showMessage", params)
--- scripts/generate_client_server.py
+++ scripts/generate_client_server.py
@@ -1,11 +1,9 @@
-"""Script to automatically generate a lanaguge client from `lsprotocol` type definitons
-"""
+"""Script to automatically generate a lanaguge client from `lsprotocol` type definitons"""
 
 import argparse
 import inspect
 import pathlib
 import re
-import sys
 import textwrap
 from typing import Optional
 from typing import Set
@@ -19,7 +17,7 @@ from lsprotocol.types import message_direction
 cli = argparse.ArgumentParser(
     description="generate language client from lsprotocol types."
 )
-cli.add_argument("-o", "--output", default=None)
+cli.add_argument("output", type=pathlib.Path)
 
 
 def write_imports(imports: Set[Tuple[str, str]]) -> str:
@@ -40,7 +38,7 @@ def to_snake_case(string: str) -> str:
     return "".join(f"_{c.lower()}" if c.isupper() else c for c in string)
 
 
-def write_notification(
+def write_client_notification(
     method: str,
     request: Type,
     params: Optional[Type],
@@ -71,6 +69,37 @@ def write_notification(
     )
 
 
+def write_server_notification(
+    method: str,
+    request: Type,
+    params: Optional[Type],
+    imports: Set[Tuple[str, str]],
+) -> str:
+    python_name = to_snake_case(method).replace("/", "_").replace("$_", "")
+
+    if params is None:
+        param_name = "None"
+        param_mod = ""
+    elif "lsprotocol" not in str(params):
+        param_mod = str(params)
+        param_name = ""
+    else:
+        param_mod, param_name = params.__module__, params.__name__
+        param_mod = param_mod.replace("lsprotocol.types", "types") + "."
+
+    return "\n".join(
+        [
+            f"def {python_name}(self, params: {param_mod}{param_name}) -> None:",
+            f'    """Send a :lsp:`{method}` notification.',
+            "",
+            textwrap.indent(inspect.getdoc(request) or "", "    "),
+            '    """',
+            f'    self.protocol.notify("{method}", params)',
+            "",
+        ]
+    )
+
+
 def get_response_type(response: Type, imports: Set[Tuple[str, str]]) -> str:
     # Find the response type.
     result_field = [f for f in response.__attrs_attrs__ if f.name == "result"][0]
@@ -88,7 +117,7 @@ def get_response_type(response: Type, imports: Set[Tuple[str, str]]) -> str:
     return result
 
 
-def write_method(
+def write_client_method(
     method: str,
     request: Type,
     params: Optional[Type],
@@ -139,6 +168,51 @@ def write_method(
     )
 
 
+def write_server_method(
+    method: str,
+    request: Type,
+    params: Optional[Type],
+    response: Type,
+    imports: Set[Tuple[str, str]],
+) -> str:
+    python_name = to_snake_case(method).replace("/", "_").replace("$_", "")
+
+    if params is None:
+        param_name = "None"
+        param_mod = ""
+    else:
+        param_mod, param_name = params.__module__, params.__name__
+        param_mod = param_mod.replace("lsprotocol.types", "types") + "."
+
+    result_type = get_response_type(response, imports)
+
+    return "\n".join(
+        [
+            f"def {python_name}(",
+            "    self,",
+            f"    params: {param_mod}{param_name},",
+            f"    callback: Optional[Callable[[{result_type}], None]] = None,",
+            ") -> Future:",
+            f'    """Make a :lsp:`{method}` request.',
+            "",
+            textwrap.indent(inspect.getdoc(request) or "", "    "),
+            '    """',
+            f'    return self.protocol.send_request("{method}", params, callback)',
+            "",
+            f"async def {python_name}_async(",
+            "    self,",
+            f"    params: {param_mod}{param_name},",
+            f") -> {result_type}:",
+            f'    """Make a :lsp:`{method}` request.',
+            "",
+            textwrap.indent(inspect.getdoc(request) or "", "    "),
+            '    """',
+            f'    return await self.protocol.send_request_async("{method}", params)',
+            "",
+        ]
+    )
+
+
 def generate_client() -> str:
     methods = []
     imports = {
@@ -159,14 +233,16 @@ def generate_client() -> str:
         request, response, params, _ = types
 
         if response is None:
-            method = write_notification(method_name, request, params, imports)
+            method = write_client_notification(method_name, request, params, imports)
         else:
-            method = write_method(method_name, request, params, response, imports)
+            method = write_client_method(
+                method_name, request, params, response, imports
+            )
 
         methods.append(textwrap.indent(method, "    "))
 
     code = [
-        "# GENERATED FROM scripts/gen-client.py -- DO NOT EDIT",
+        "# GENERATED FROM scripts/generate_client_server.py -- DO NOT EDIT",
         "# flake8: noqa",
         write_imports(imports),
         "",
@@ -190,18 +266,74 @@ def generate_client() -> str:
     return "\n".join(code)
 
 
+def generate_server() -> str:
+    methods = []
+    imports = {
+        "typing",
+        ("concurrent.futures", "Future"),
+        ("lsprotocol", "types"),
+        ("cattrs", "Converter"),
+        ("pygls.protocol", "LanguageServerProtocol"),
+        ("pygls.protocol", "default_converter"),
+        ("pygls.server", "Server"),
+        ("typing", "Callable"),
+        ("typing", "Optional"),
+        ("typing", "Type"),
+    }
+
+    for method_name, types in METHOD_TO_TYPES.items():
+        # Skip any requests that come from the client.
+        if message_direction(method_name) == "clientToServer":
+            continue
+
+        request, response, params, _ = types
+
+        if response is None:
+            method = write_server_notification(method_name, request, params, imports)
+        else:
+            method = write_server_method(
+                method_name, request, params, response, imports
+            )
+
+        methods.append(textwrap.indent(method, "    "))
+
+    code = [
+        "# GENERATED FROM scripts/generate_client_server.py -- DO NOT EDIT",
+        "# flake8: noqa",
+        write_imports(imports),
+        "",
+        "",
+        "class BaseLanguageServer(Server):",
+        "",
+        "    protocol: LanguageServerProtocol",
+        "",
+        "    def __init__(",
+        "        self,",
+        "        protocol_cls: Type[LanguageServerProtocol] = LanguageServerProtocol,",
+        "        converter_factory: Callable[[], Converter] = default_converter,",
+        "        loop=None,",
+        "        **kwargs,",
+        "    ):",
+        "        super().__init__(protocol_cls, converter_factory, loop, **kwargs)",
+        "",
+        *methods,
+    ]
+    return "\n".join(code)
+
+
 def main():
     args = cli.parse_args()
 
     # Make sure all the type annotations in lsprotocol are resolved correctly.
     _resolve_forward_references()
-    client = generate_client()
 
-    if args.output is None:
-        sys.stdout.write(client)
-    else:
-        output = pathlib.Path(args.output)
-        output.write_text(client)
+    client = generate_client()
+    output = args.output / "_base_client.py"
+    output.write_text(client)
+
+    server = generate_server()
+    output = args.output / "_base_server.py"
+    output.write_text(server)
 
 
 if __name__ == "__main__":
