87e1033fe5276e265237948aada10b77857a503b
Alex Carney <alcarneyme@gmail.com>
2025-05-17 19:23:06 +0100

    feat: use type annotations to parse command arguments

---
+++ examples/servers/commands.py
@@ -0,0 +1,153 @@
+############################################################################
+# Copyright(c) Open Law Library. All rights reserved.                      #
+# See ThirdPartyNotices.txt in the project root for additional notices.    #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License")           #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http: // www.apache.org/licenses/LICENSE-2.0                         #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+"""This demonstrates the different ways in which server commands can be registered.
+"""
+from __future__ import annotations
+
+import logging
+import math
+
+import attrs
+from lsprotocol import types
+
+from pygls.cli import start_server
+from pygls.lsp.server import LanguageServer
+
+server = LanguageServer("commands-server", "v1")
+
+
+@server.command("random")
+def random():
+    """Accepts no arguments, returns a random number"""
+    return 4  # https://xkcd.com/221/
+
+
+@server.command("random.wrapped")
+def random_wrapped(ls):
+    """Accepts no arguments (but uses an injected server instance).
+    Returns a random number"""
+    ls.window_log_message(
+        types.LogMessageParams(type=types.MessageType.Info, message="running...")
+    )
+    return 4
+
+
+@server.command("calculate.sum")
+def calculate_sum(*args):
+    """Using *args to accept any number of arguments"""
+    logging.info("arguments: %r", args)
+    return sum(args)
+
+
+@server.command("calculate.sum.wrapped")
+def calculate_sum_wrapped(s: LanguageServer, *args):
+    """Using *args to accept any number of arguments"""
+    s.window_log_message(
+        types.LogMessageParams(type=types.MessageType.Info, message=f"{args=}")
+    )
+    return calculate_sum(*args)
+
+
+@server.command("calculate.pow")
+def calculate_pow(x: float, n):
+    """"""
+    logging.info("x: %r, n: %r", x, n)
+    return x**n
+
+
+@server.command("calculate.pow.wrapped")
+def calculate_pow_wrapped(s: LanguageServer, x, n: int):
+    """Using *args to accept any number of arguments"""
+    s.window_log_message(
+        types.LogMessageParams(type=types.MessageType.Info, message=f"{x=}, {n=}")
+    )
+    return calculate_pow(x, n)
+
+
+@server.command("calculate.div")
+def calculate_div(x: float, n):
+    """"""
+    logging.info("x: %r, n: %r", x, n)
+    return x / n
+
+
+@server.command("calculate.div.wrapped")
+def calculate_div_wrapped(s: LanguageServer, x, n: float):
+    """Using *args to accept any number of arguments"""
+    s.window_log_message(
+        types.LogMessageParams(type=types.MessageType.Info, message=f"{x=}, {n=}")
+    )
+    return calculate_div(x, n)
+
+
+@attrs.define
+class Triangle:
+    """A triangle."""
+
+    a: float
+    """Length of side a"""
+
+    b: float
+    """Length of side b"""
+
+    c: float = attrs.field(default=0)
+    """Length of side c - the hypotenuse"""
+
+
+@server.command("calculate.triangle.hypotenuse")
+def calculate_triangle_hypotenuse(triangle: Triangle):
+    """When type annotations are given, pygls will automatically convert compatible
+    values to their attrs-based representation"""
+    logging.info("triangle: %r", triangle)
+
+    triangle.c = math.sqrt(triangle.a**2 + triangle.b**2)
+    return triangle
+
+
+@server.command("calculate.triangle.hypotenuse.wrapped")
+def calculate_triangle_hypotenuse_wrapped(ls: LanguageServer, triangle: Triangle):
+    """When type annotations are given, pygls will automatically convert compatible
+    values to their attrs-based representation"""
+    ls.window_log_message(
+        types.LogMessageParams(type=types.MessageType.Info, message=f"{triangle=}")
+    )
+    return calculate_triangle_hypotenuse(triangle)
+
+
+@server.command("calculate.untyped.hypotenuse")
+def calculate_untyped_hypotenuse(tri):
+    """Calculate the hypotenuse of a right-angled triangle"""
+    logging.info("tri: %r", tri)
+
+    a = tri["a"]
+    b = tri["b"]
+    c = math.sqrt(a**2 + b**2)
+    return dict(a=a, b=b, c=c)
+
+
+@server.command("calculate.untyped.hypotenuse.wrapped")
+def calculate_untyped_hypotenuse_wrapped(ls: LanguageServer, tri):
+    """Calculate the hypotenuse of a right-angled triangle"""
+    ls.window_log_message(
+        types.LogMessageParams(type=types.MessageType.Info, message=f"{tri=}")
+    )
+    return calculate_untyped_hypotenuse(tri)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    start_server(server)
---
+++ tests/e2e/test_commands.py
@@ -0,0 +1,317 @@
+############################################################################
+# Copyright(c) Open Law Library. All rights reserved.                      #
+# See ThirdPartyNotices.txt in the project root for additional notices.    #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License")           #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http: // www.apache.org/licenses/LICENSE-2.0                         #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+from __future__ import annotations
+
+import logging
+import itertools
+import typing
+
+import pytest
+import pytest_asyncio
+from lsprotocol import types
+
+from pygls.exceptions import JsonRpcInternalError, JsonRpcInvalidParams
+
+if typing.TYPE_CHECKING:
+    from typing import Tuple
+
+    from pygls.lsp.client import LanguageClient
+
+
+logger = logging.getLogger(__name__)
+
+
+@pytest_asyncio.fixture(scope="module", loop_scope="module")
+async def commands(get_client_for):
+    async for result in get_client_for("commands.py"):
+        client, _ = result
+
+        @client.feature(types.WINDOW_LOG_MESSAGE)
+        def log(params: types.LogMessageParams):
+            logger.info(params.message)
+
+        yield result
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_command_not_defined(
+    commands: Tuple[LanguageClient, types.InitializeResult]
+):
+    """Ensure that the example commands server handles the case where
+    the requested command is not defined."""
+
+    client, _ = commands
+
+    with pytest.raises(
+        JsonRpcInvalidParams,
+        match="Invalid Params: Command name 'not.defined' is not defined",
+    ):
+        await client.workspace_execute_command_async(
+            types.ExecuteCommandParams(
+                command="not.defined",
+            )
+        )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize("name", ["random", "random.wrapped"])
+async def test_random(
+    commands: Tuple[LanguageClient, types.InitializeResult], name: str
+):
+    """Ensure that the example commands server can execute both the wrapped and
+    unwrapped ``calculate.triangle.hypotenuse`` commands correctly."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    result = await client.workspace_execute_command_async(
+        types.ExecuteCommandParams(
+            command=name,
+            arguments=None,
+        )
+    )
+
+    assert result == 4
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize("name", ["random", "random.wrapped"])
+async def test_random_invalid(
+    commands: Tuple[LanguageClient, types.InitializeResult], name: str
+):
+    """Ensure that the example commands server handles the case when a command is called
+    with too many arguments."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    with pytest.raises(
+        JsonRpcInvalidParams, match="Invalid Params: Expected 0 arguments, got 1"
+    ):
+        await client.workspace_execute_command_async(
+            types.ExecuteCommandParams(
+                command=name,
+                arguments=[{"seed": 1234}],
+            )
+        )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize(
+    "name, setup",
+    [
+        *itertools.product(
+            ["calculate.sum", "calculate.sum.wrapped"],
+            [
+                ([], 0),
+                ([1], 1),
+                ([1, 2], 3),
+                ([1, 2, 3], 6),
+            ],
+        ),
+    ],
+)
+async def test_calculate_sum(
+    commands: Tuple[LanguageClient, types.InitializeResult],
+    name: str,
+    setup: tuple[list[int], int],
+):
+    """Ensure that the example commands server can execute both the wrapped and
+    unwrapped ``calculate.sum`` commands correctly."""
+
+    client, initialize_result = commands
+    arguments, expected = setup
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    result = await client.workspace_execute_command_async(
+        types.ExecuteCommandParams(
+            command=name,
+            arguments=arguments,
+        )
+    )
+
+    assert result == expected
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize("name", ["calculate.pow", "calculate.pow.wrapped"])
+async def test_calculate_pow_invalid(
+    commands: Tuple[LanguageClient, types.InitializeResult], name: str
+):
+    """Ensure that the example commands server handles the case where not enough
+    arguments are given."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    with pytest.raises(
+        JsonRpcInvalidParams, match="Invalid Params: Expected 2 arguments, got 1"
+    ):
+        await client.workspace_execute_command_async(
+            types.ExecuteCommandParams(
+                command=name,
+                arguments=[3],
+            )
+        )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize(
+    "name",
+    ["calculate.pow", "calculate.pow.wrapped"],
+)
+async def test_calculate_pow(
+    commands: Tuple[LanguageClient, types.InitializeResult],
+    name: str,
+):
+    """Ensure that the example commands server can execute both the wrapped and
+    unwrapped ``calculate.pow`` commands correctly."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    result = await client.workspace_execute_command_async(
+        types.ExecuteCommandParams(
+            command=name,
+            arguments=[2, 3],
+        )
+    )
+
+    assert result == 8
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize("name", ["calculate.div", "calculate.div.wrapped"])
+async def test_calculate_div_invalid(
+    commands: Tuple[LanguageClient, types.InitializeResult], name: str
+):
+    """Ensure that the example commands server handles the case where
+    there is a error in the command handler."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    with pytest.raises(
+        JsonRpcInternalError, match="ZeroDivisionError: float division by zero"
+    ):
+        await client.workspace_execute_command_async(
+            types.ExecuteCommandParams(
+                command=name,
+                arguments=[3, 0],
+            )
+        )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize(
+    "name",
+    ["calculate.div", "calculate.div.wrapped"],
+)
+async def test_calculate_div(
+    commands: Tuple[LanguageClient, types.InitializeResult],
+    name: str,
+):
+    """Ensure that the example commands server can execute both the wrapped and
+    unwrapped ``calculate.pow`` commands correctly."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    result = await client.workspace_execute_command_async(
+        types.ExecuteCommandParams(
+            command=name,
+            arguments=[6, 3],
+        )
+    )
+
+    assert result == 2
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize(
+    "name",
+    [
+        "calculate.triangle.hypotenuse",
+        "calculate.triangle.hypotenuse.wrapped",
+        "calculate.untyped.hypotenuse",
+        "calculate.untyped.hypotenuse.wrapped",
+    ],
+)
+async def test_calculate_hypotenuse(
+    commands: Tuple[LanguageClient, types.InitializeResult], name: str
+):
+    """Ensure that the example commands server can execute both the wrapped and
+    unwrapped ``calculate.triangle.hypotenuse`` commands correctly."""
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    result = await client.workspace_execute_command_async(
+        types.ExecuteCommandParams(
+            command=name,
+            arguments=[{"a": 3, "b": 4}],
+        )
+    )
+
+    assert result == {"a": 3, "b": 4, "c": 5}
+
+
+@pytest.mark.asyncio(loop_scope="module")
+@pytest.mark.parametrize(
+    "name",
+    [
+        "calculate.triangle.hypotenuse",
+        "calculate.triangle.hypotenuse.wrapped",
+    ],
+)
+async def test_calculate_hypotenuse_invalid(
+    commands: Tuple[LanguageClient, types.InitializeResult], name: str
+):
+    """Ensure that the example commands server handles the case where the given argument
+    does not align to the expected type
+    """
+
+    client, initialize_result = commands
+
+    provider = initialize_result.capabilities.execute_command_provider
+    assert name in provider.commands
+
+    with pytest.raises(
+        JsonRpcInvalidParams, match="Invalid Params: While structuring Triangle.*"
+    ):
+        await client.workspace_execute_command_async(
+            types.ExecuteCommandParams(
+                command=name,
+                arguments=[{"x": 3, "y": 4}],
+            )
+        )
