58b8c1a0483a23e8782a50e2988fc7d67199d935
Alex Carney <alcarneyme@gmail.com>
2026-09-10 12:12:38 +0100

    chore: update devcontainer versions

--- .devcontainer/tools.mk
+++ .devcontainer/tools.mk
@@ -5,8 +5,8 @@ ifeq ($(strip $(ARCH)),)
 $(error Unable to determine platform architecture)
 endif
 
-NODE_VERSION := 20.19.3
-UV_VERSION := 0.7.16
+NODE_VERSION := 24.21.0
+UV_VERSION := 0.12.10
 
 UV ?= $(shell command -v uv)
 UVX ?= $(shell command -v uvx)
